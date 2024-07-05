536cea862e72f37ecdf748d96026f94be8cf4fe5
khorlim <khorlim2001@gmail.com>
2024-07-05 18:16:50 +0800

    rename

--- lib/src/post_api.dart
+++ lib/src/post_api.dart
@@ -5,13 +5,11 @@ import 'package:base_api/src/enum/http_method.dart';
 import 'package:http/http.dart';
 import 'package:http/src/request.dart';
 
-class PostApi extends BaseApi {
+class PostFormApi extends BaseApi {
   final Map<String, String> bodyFields;
-  final Map<String, dynamic> bodyJson;
-  PostApi({
+  PostFormApi({
     required super.uri,
     this.bodyFields = const {},
-    this.bodyJson = const {},
     super.method = HttpMethod.post,
     required super.headers,
   });
@@ -20,7 +18,6 @@ class PostApi extends BaseApi {
   BaseRequest get httpRequest {
     final request = Request(method.toString(), uri);
     request.bodyFields = bodyFields;
-    request.body = json.encode(bodyJson);
     request.headers.addAll(headers);
     return request;
   }
